ad16c1360c7b3205c35575046962e221800dee51
D-Bars <awotik26@gmail.com>
2025-03-21 01:50:44 +0100

    stored generated cards array in the gameState

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 import PlayerSetupPanel from "./components/PlayerSetupPanel"
+import { useGameStore } from "./store/gameStore";
+import { GenerateCardsArray } from "./utils/GenerateCardsArray"
 
 function App() {
-
+  useEffect(() => {
+    const cardArray = GenerateCardsArray();
+    useGameStore.getState().setCards(cardArray);
+  }, []);
   return (
     <div>
       <PlayerSetupPanel></PlayerSetupPanel>
