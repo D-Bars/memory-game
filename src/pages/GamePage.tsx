4c106a55e91cb651d9adf713732764bddbffcd0e
D-Bars <awotik26@gmail.com>
2025-03-23 16:23:38 +0100

    added: backToSetup in gamePage

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,17 +1,26 @@
 import { useGameStore } from '../store/gameStore';
 import GameBoard from '../components/GameBoard';
+import cl from './styles/GamePage.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 const GamePage = () => {
-  const { isGameStart } = useGameStore();
-  return (
-    <div>
-      {isGameStart ? (
-        <GameBoard />
-      ) : (
-        <h2>Game has not started yet. Please go to the setup page.</h2>
-      )}
-    </div>
-  );
+    const { isGameStart } = useGameStore();
+    const navigate = useNavigate();
+    const backToSetup = () => {
+        navigate('/');
+    }
+    return (
+        <div>
+            {isGameStart ? (
+                <GameBoard />
+            ) : (
+                <div className={cl.route_box}>
+                    <h2>Game has not started yet. Please go to the</h2>
+                    <div className={cl.route} onClick={backToSetup}>setup page.</div>
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default GamePage;
